36a2ae2a397abaefa17d5e7d05291eeb737fc902
blade8323 <blade8323@mail.ru>
2020-09-07 21:54:26 +0500

    final

--- PinchMe/ViewController.swift
+++ PinchMe/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    private var imageView:UIImageView!
+    private var scale = CGFloat(1)
+    private var previousScale = CGFloat(1)
+    private var rotation = CGFloat(0)
+    private var previousRotation = CGFloat(0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let image = UIImage(named: "yosemite-meadows")
+        imageView = UIImageView(image: image)
+        imageView.isUserInteractionEnabled = true
+        imageView.center = view.center
+        view.addSubview(imageView)
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        pinchGesture.delegate = self
+        imageView.addGestureRecognizer(pinchGesture)
+        
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(ViewController.doRotate(_:)))
+        rotationGesture.delegate = self
+        imageView.addGestureRecognizer(rotationGesture)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 
+    func transformImageView() {
+        var t = CGAffineTransform(scaleX: scale * previousScale, y: scale * previousScale)
+        t = t.rotated(by: rotation + previousRotation)
+        imageView.transform = t
+    }
+    
+    @objc func doPinch(_ gesture: UIPinchGestureRecognizer) {
+        scale = gesture.scale
+        transformImageView()
+        if gesture.state == .ended {
+            previousScale = scale * previousScale
+            scale = 1
+        }
+    }
+    
+    @objc func doRotate(_ gesture: UIRotationGestureRecognizer) {
+        rotation = gesture.rotation
+        transformImageView()
+        if gesture.state == .ended {
+            previousRotation = rotation + previousRotation
+            rotation = 0
+        }
+    }
 
 }
 
